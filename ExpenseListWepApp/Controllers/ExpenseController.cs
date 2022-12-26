b28d288a13c1f77a9ac15d1a2659ff43e411251f
ceynur aydın <ceynur.aydin@gmail.com>
2022-12-26 22:08:39 +0300

    index action ve index view tamamlandı.

--- ExpenseListWepApp/Controllers/ExpenseController.cs
+++ ExpenseListWepApp/Controllers/ExpenseController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseListWepApp.Data;
+using ExpenseListWepApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseListWepApp.Controllers
 {
     public class ExpenseController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public ExpenseController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Expense> ExpenseList = _db.Expenses.Select(e=>e);
+            return View(ExpenseList);
         }
+
     }
 }
