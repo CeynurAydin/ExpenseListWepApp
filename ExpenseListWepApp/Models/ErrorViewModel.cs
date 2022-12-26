11dc1976ec220777b2f0d9270ac3fd742e973695
ceynur aydın <ceynur.aydin@gmail.com>
2022-12-26 20:41:46 +0300

    Add project files.

---
+++ ExpenseListWepApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpenseListWepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
