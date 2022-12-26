9877570067a371eda61aadd15df53fc7127cc7f4
ceynur aydın <ceynur.aydin@gmail.com>
2022-12-26 21:09:50 +0300

    Model ve DbContext eklendi. Entity Framework Core-EFCore SqlServer-EFTools Packages yüklendi. Program.cs'de Sql bağlantısı yapıldı. appsettings'e connectionStrings eklendi. Migration işlemi yapıldı.

---
+++ ExpenseListWepApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseListWepApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseListWepApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ ExpenseListWepApp/Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ExpenseListWepApp.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Expense Name")]
+        public string ExpenseName { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "You entered wrong value")]
+        public int Amount { get; set; }
+
+    }
+}
