cd7367c28d1235ba3d393bfce9af15aba87ffe70
ceynur aydın <ceynur.aydin@gmail.com>
2022-12-28 18:51:43 +0300

    Upsert post ve Upsert get actions tamamlandı. Upsert view tamamlandı.

--- ExpenseListWepApp/Controllers/ExpenseController.cs
+++ ExpenseListWepApp/Controllers/ExpenseController.cs
@@ -1,6 +1,7 @@
 ﻿using ExpenseListWepApp.Data;
 using ExpenseListWepApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ExpenseListWepApp.Controllers
 {
@@ -16,6 +17,45 @@ namespace ExpenseListWepApp.Controllers
         {
             IEnumerable<Expense> ExpenseList = _db.Expenses.Select(e=>e);
             return View(ExpenseList);
+        }
+        public IActionResult Upsert(int id)
+        {
+            Expense expense = new Expense();
+            if (id==0)
+            {
+                return View(expense);
+            }
+            else
+            {
+                expense=_db.Expenses.Find(id);
+                return View(expense);
+            }
+
+           
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                if (expense.Id==0)
+                {
+                    _db.Expenses.Add(expense);
+                }
+                else 
+                {
+                    _db.Update(expense);
+                
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(expense);
+
+
         }
 
     }
